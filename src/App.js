0009a6d8afab9dd6680e9f99b2be792dd72a0f11
Seo-Pt <seonah.pari.pt@gmail.com>
2022-11-12 15:57:30 +0000

    Successful data fetch and display inside main App component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from "react";
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  const [list, setList] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3008/api/tasks/").then(response => response.json()).then(response => setList(response));
-  }, []);
-
-   console.log(list);
-  return (
-    <div className="App">
-
-      <header className="App-header">
-      <p>Test To-Do List</p>
-      </header>
-        {list.map(function(task, index) {
-
+    const [result, setResult] = useState([]);
+    const [fetched, setFetched] = useState(false);
+    let status = "";
+    let listItem = document.getElementsByClassName("task");
+  
+    //const completion = [true, false];
+   // const [completion, setCompletion] = useState(" = ");
+   // console.log(list);
+  
+    useEffect(() => {
+      fetch("http://localhost:3008/api/tasks/").then(response => response.json()).then(response => {
+          console.log(response)
+          
+          if (response.length > 0) {
+              setResult(response);
+      } else {
+          console.log("failed call");
+      }
+      }).then(setFetched(true))
+    }, []);
+    
+    console.log(result);
+    if (fetched) {
       return (
-        <ul key={index}>
-          <li>{task.text} Hey</li>
-        </ul>
+        <div className="App">
+        <header className="App-header">
+          <p>Test To-Do List</p>
+          </header>
+          <ul className="App-list">
+          {result.map(function(task, index) {
+            if (task.completed === false) {
+              status = "Incomplete";
+              //listItem.classList.add("red");
+            } else {
+              status = "Complete";
+              
+            }
+            //console.log(task);
+      return (
+          <li key={index} id="listItem" className={task.completed === false ? "red" : "green"} >{task.text}---{status}</li>
       );
     })}
+    </ul>
     </div>
-  );
+    );
+  }
 }
-
