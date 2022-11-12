18fb5ff0d39215ca72cbbe05c45948b7617ed8fa
Seo-Pt <seonah.pari.pt@gmail.com>
2022-11-12 19:53:52 +0000

    Squashed commit of the following:
    commit 8d39ab68bba48fa49f496431c7f0c896bad02236
    Author: Seo-Pt <seonah.pari.pt@gmail.com>
    Date:   Sat Nov 12 19:50:13 2022 +0000
    
        Updated className
    
    commit 89213d6059ca5a18b06e91c3044c04a5295d1fd8
    Author: Seo-Pt <seonah.pari.pt@gmail.com>
    Date:   Sat Nov 12 19:49:20 2022 +0000
    
        Added error display for user if fetch is unsuccessful, changed status color instead of task
    
    commit e9582920fbdc70b69ebdc4d643c3ebc43b875742
    Author: Seo-Pt <seonah.pari.pt@gmail.com>
    Date:   Sat Nov 12 18:57:55 2022 +0000
    
        Migrated fetch request and states to TaskList component, conditional component rendering
    
    commit ddd8ced377440742f158a3bfe50a8dc7e6027434
    Author: Seo-Pt <seonah.pari.pt@gmail.com>
    Date:   Sat Nov 12 18:13:14 2022 +0000
    
        Added catch method to fetch

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
-export default function TaskList(props) {
-    let display = props.display;
-    let listLength = props.result.length;
-    let status = "";
-    let result = props.result;
+export default function TaskList() {
+    const [result, setResult] = useState([]);
+    const [success, setSuccess] = useState(true);
+    const listLength = result.length;
 
-    if (display === true) {
+    useEffect(() => {
+        fetch("http://localhost:3008/api/tasks/").then(response => response.json())
+        .then(response => setResult(response))
+        .catch(() => setSuccess(false));
+      }, []);
+
         return(
-            <section className="Task-list-section">
-                <h3 className="mt-4">Task List ({listLength} in total)</h3>
+            <>
+            <section hidden={success} className="Task-list-section mt-4">
                 <ul className="Task-list">
-                    {result.map(function(task, index) {
-                        if (task.completed === false) {
-                        status = "Incomplete";
-                        } else {
-                        status = "Complete";
-                        }
+                    <li>Error: There was an issue fetching tasks from the API</li>
+                </ul>
+            </section>
+
+            <section hidden={!success} className="Task-list-section">
+                <h3 className="mt-4">Task List ({listLength} in total) </h3>
+                <ul className="Task-list">
+                    {result.map(function(task) {
                     return (
-                    <li key={index}>{task.text}<span className={task.completed === false ? "Task-list-status red " : "Task-list-status green"}>-----{status}</span></li>
-                    
+                    <li key={task.id} >{task.text}<span className={task.completed === false ? "Task-list-status red" : "Task-list-status green"}>-----{task.completed ? "Completed" : "Incomplete"}</span></li>
                     );
                     })}
                 </ul>
             </section>
+            </>
         );
-    } else {
-        return null
-    }
 }
